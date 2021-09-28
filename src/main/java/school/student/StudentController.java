aa758b5f662d72807a6e65c68cac13e55c922412
sunabove <sunabove@users.noreply.github.com>
2021-09-28 13:21:47 +0900

    **

--- src/main/java/school/student/StudentController.java
+++ src/main/java/school/student/StudentController.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012-2019 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package school.student;
 
 import org.springframework.stereotype.Controller;
@@ -31,12 +16,6 @@ import javax.validation.Valid;
 import java.util.Collection;
 import java.util.Map;
 
-/**
- * @author Juergen Hoeller
- * @author Ken Krebs
- * @author Arjen Poutsma
- * @author Michael Isvy
- */
 @Controller
 class StudentController {
 
@@ -76,8 +55,8 @@ class StudentController {
 
 	@GetMapping("/students/find")
 	public String initFindForm(Map<String, Object> model) {
-		model.put("owner", new Student());
-		return "students/findOwners";
+		model.put("student", new Student());
+		return "students/findStudents";
 	}
 
 	@GetMapping("/students")
@@ -93,7 +72,7 @@ class StudentController {
 		if (results.isEmpty()) {
 			// no students found
 			result.rejectValue("lastName", "notFound", "not found");
-			return "students/findOwners";
+			return "students/findStudents";
 		}
 		else if (results.size() == 1) {
 			// 1 owner found
@@ -103,7 +82,7 @@ class StudentController {
 		else {
 			// multiple students found
 			model.put("selections", results);
-			return "students/ownersList";
+			return "students/studentsList";
 		}
 	}
 
